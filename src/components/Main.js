e72572a74dd215aa94974af9347841ccf7c76ae0
Lance Lopez <lancelopez46@gmail.com>
2023-03-23 19:49:42 +0800

    add levels

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,12 @@ import { useEffect, useState } from "react";
 
 const Main = () => {
   const [list, setList] = useState([]);
-  const [pokeQuantity, setPokeQuantity] = useState(6);
-  const [pokeID, setPokeID] = useState([...Array(150).keys()]);
+  const [pokeQuantity, setPokeQuantity] = useState(1);
+  const [pokeID, setPokeID] = useState([...Array(151).keys()]);
   const [score, setScore] = useState(0);
-  const [clickedCards, setClickedCards] = useState([]);
   const [bestScore, setBestScore] = useState(0);
-  const [isLoading, setisLoading] = useState(false);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchPokemonAPI(id) {
     try {
@@ -21,6 +21,7 @@ const Main = () => {
         image: pokeResponse.sprites.front_default,
       };
       setList((prev) => [...prev, pokeObject]);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -28,15 +29,46 @@ const Main = () => {
 
   function fetchPokemons() {
     for (let i = 0; i < pokeQuantity; i++) {
-      const randomIndex = Math.floor(Math.random() * pokeID.length) + 1;
-      setPokeID(...pokeID, pokeID.splice(randomIndex, 1));
-      fetchPokemonAPI(pokeID[randomIndex]);
+      const randomIndex = Math.floor(Math.random() * pokeID.length);
+      fetchPokemonAPI(pokeID[randomIndex] + 1);
+      setPokeID([...pokeID, pokeID.splice(randomIndex, 1)]);
     }
   }
 
   useEffect(() => {
-    fetchPokemons();
+    setIsLoading(true);
+    // add timeout to simulate loading
+    setTimeout(() => {
+      fetchPokemons();
+    }, 300);
   }, [pokeQuantity]);
+
+  useEffect(() => {
+    if (clickedCards.length === pokeQuantity) {
+      setClickedCards([]);
+      setList([]);
+      setPokeQuantity((prev) => prev + 1);
+    }
+    if (score > bestScore) setBestScore(score);
+  }, [clickedCards, score, bestScore, pokeQuantity]);
+
+  function handleCardClick(name) {
+    setList(list.sort((a, b) => 0.5 - Math.random()));
+    setClickedCards([...clickedCards, name]);
+    setScore(score + 1);
+
+    if (clickedCards.includes(name)) {
+      gameOver();
+    }
+  }
+
+  function gameOver() {
+    setScore(0);
+    setClickedCards([]);
+    setPokeQuantity(1);
+    setList([]);
+    setPokeID([...Array(151).keys()]);
+  }
 
   return (
     <main>
@@ -47,23 +79,17 @@ const Main = () => {
           <div className="score-board">
             <span>Score: {score}</span>
             <span>Best Score: {bestScore}</span>
+            <h1>Level: {pokeQuantity}</h1>
           </div>
 
           <div className="card-container">
             {list.map((character) => {
               return (
                 <Card
+                  key={character.id}
                   name={character.name}
                   image={character.image}
-                  key={character.id}
-                  clickedCards={clickedCards}
-                  setClickedCards={setClickedCards}
-                  score={score}
-                  setScore={setScore}
-                  bestScore={bestScore}
-                  setBestScore={setBestScore}
-                  list={list}
-                  setList={setList}
+                  handleCardClick={handleCardClick}
                 />
               );
             })}
--- src/components/body/Card.js
+++ src/components/body/Card.js
@@ -1,27 +1,10 @@
 const Card = (props) => {
-  function handleClick() {
-    if (props.clickedCards.includes(props.name)) {
-      console.log("game over");
-      if (props.score > props.bestScore) {
-        props.setBestScore(props.score);
-      }
-      props.setScore(0);
-      props.setClickedCards([]);
-      return;
-    }
-    console.log(props.list);
-    const shuffledArray = props.list.sort((a, b) => 0.5 - Math.random());
-    console.log(shuffledArray);
-
-    props.setList(shuffledArray);
-    props.setClickedCards([...props.clickedCards, props.name]);
-    props.setScore(props.score + 1);
-  }
+  const { name, image, handleCardClick } = props;
 
   return (
-    <div className="card" onClick={handleClick}>
-      <img className="disable-pointer" src={props.image} alt={props.name} />
-      <p>{props.name}</p>
+    <div className="card" onClick={() => handleCardClick(name)}>
+      <img className="disable-pointer" src={image} alt={name} />
+      <p>{name}</p>
     </div>
   );
 };
